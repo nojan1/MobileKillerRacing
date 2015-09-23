37305754172e043b78c99db5ccf6dfc431c27796
Niklas Hedlund <nojan1989@gmail.com>
2015-09-23 22:43:43 +0200

    Training level can now be started from menu

---
+++ Assets/Scripts/MainMenuHandler.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Events;
+
+public class MainMenuHandler : MonoBehaviour {
+    public void StartPractice()
+    {
+        Application.LoadLevel("training"); 
+    }
+}
