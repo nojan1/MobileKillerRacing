57a8367a39083ab585ae47b0599775ef811afc67
Niklas Hedlund <nojan1989@gmail.com>
2015-09-23 18:28:25 +0200

    Added simple test menu screen

---
+++ Assets/Scripts/RotatingObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingObject : MonoBehaviour {
+
+    public float rotationSpeed = 6f; 
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0f, rotationSpeed * Time.deltaTime, 0f));
+	}
+}
